f4ad9eb6b93c1f2f38e387bf613acb3165604966
Simon de la Rouviere <simon@delarouviere.com>
2015-05-20 20:18:43 +0200

    Refactor to a more generic setup, making index.html almost closer an IDE. Examples, in the future will focus on using the reactor as a framework.

--- script.jsx
+++ script.jsx
@@ -14,30 +14,35 @@ var urlParams;
 })();
 /*------------------*/
 
-var abi;
 var address;
 if("address" in urlParams) {
     address = urlParams['address'];
 } else {
-    var address = '0xa206c162996a2ee95455991f566ee4b376d52445'; //change this to a real address if it has been pushed to the network.
+    var address = '0xa206c162996a2ee95455991f566ee4b376d52445'; //random default
+}
+
+var contract;
+if("contract" in urlParams) {
+    contract = urlParams['contract'];
+} else {
+    var contract = "contracts/Basic_Test.sol"; //default
 }
 
 
-console.log(address);
-console.log(web3.eth.coinbase);
 $.ajax({
-  url: "Basic_Test.abi",
-  dataType: 'json',
-  cache: false,
-  success: function(data) {
-        abi = data;
+    url: contract,
+    dataType: 'text',
+    cache: false,
+    success: function(data) {
+        compiled = web3.eth.compile.solidity(data);
+        console.log(compiled.info.abiDefinition);
+        var abi = compiled.info.abiDefinition;
         $.each(abi, function(i, obj) {
-              //use obj.id and obj.name here, for example:
-              console.log(obj);
+            //use obj.id and obj.name here, for example:
+            console.log(obj);
         });
         var Mycontract = web3.eth.contract(abi);
         var instance = Mycontract.at(address);
         React.render(<Reactor abi={abi} instance={instance}/>, document.getElementById('contract'));
-  }
+  },
 });
-
